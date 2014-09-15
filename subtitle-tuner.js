c2931476529e9e49f8c19a78a95fdd9736ab5e7d
Alex Olshansky <i@creagenics.com>
2014-09-15 23:19:30 +0200

    Refactor (smaller functions)

--- subtitle-tuner.js
+++ subtitle-tuner.js
@@ -41,30 +41,42 @@ var readSrtFile = function(fileName) {
 };
 
 var parseSrtFile = function(fileName, data) {
-  var newData = data.replace(timestampPattern, function(match, p1, p2, p3, p4) {
-    p1 = parseInt(p1, 10);
-    p2 = parseInt(p2, 10);
-    p3 = parseInt(p3, 10);
-    p4 = parseInt(p4, 10);
-
-    var milliseconds = p1 * 3600000 + p2 * 60000 + p3 * 1000 + p4;
-
-    milliseconds = milliseconds / 1.0442 + 350;
-
-    p1 = Math.floor(milliseconds / 3600000);
-    p2 = Math.floor((milliseconds - p1 * 3600000) / 60000);
-    p3 = Math.floor((milliseconds - p1 * 3600000 - p2 * 60000) / 1000);
-    p4 = Math.floor(milliseconds - p1 * 3600000 - p2 * 60000 - p3 * 1000);
-
-    p1 = ('0' + p1).substr(-2);
-    p2 = ('0' + p2).substr(-2);
-    p3 = ('0' + p3).substr(-2);
-    p4 = ('00' + p4).substr(-3);
-
-    return p1 + ':' + p2 + ':' + p3 + ',' + p4;
-  });
-
+  var newData = data.replace(
+    timestampPattern,
+    function(timestamp, p1, p2, p3, p4) {
+      var milliseconds = convertTimestampToMilliseconds(p1, p2, p3, p4);
+      milliseconds = updateTime(milliseconds, 1 / 1.0442, 350);
+      return convertMillisecondsToTimestamp(milliseconds);
+    }
+  );
   fs.writeFileSync(fileName, newData);
+};
+
+var convertTimestampToMilliseconds = function(p1, p2, p3, p4) {
+  p1 = parseInt(p1, 10);
+  p2 = parseInt(p2, 10);
+  p3 = parseInt(p3, 10);
+  p4 = parseInt(p4, 10);
+
+  return p1 * 3600000 + p2 * 60000 + p3 * 1000 + p4;
+};
+
+var convertMillisecondsToTimestamp = function(milliseconds) {
+  var p1 = Math.floor(milliseconds / 3600000);
+  var p2 = Math.floor((milliseconds - p1 * 3600000) / 60000);
+  var p3 = Math.floor((milliseconds - p1 * 3600000 - p2 * 60000) / 1000);
+  var p4 = Math.floor(milliseconds - p1 * 3600000 - p2 * 60000 - p3 * 1000);
+
+  p1 = ('0' + p1).substr(-2);
+  p2 = ('0' + p2).substr(-2);
+  p3 = ('0' + p3).substr(-2);
+  p4 = ('00' + p4).substr(-3);
+
+  return p1 + ':' + p2 + ':' + p3 + ',' + p4;
+};
+
+var updateTime = function(x, a, b) {
+  return x * a + b;
 };
 
 readSrtDir();
